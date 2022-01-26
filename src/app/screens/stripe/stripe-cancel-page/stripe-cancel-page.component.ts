6e94c4f82184f310856392c83cd3a37c58b1c9a1
unknown <jamelyassin84@gmail.com>
2022-01-25 21:03:43 -0800

    Cancel Page

--- src/app/screens/stripe/stripe-cancel-page/stripe-cancel-page.component.ts
+++ src/app/screens/stripe/stripe-cancel-page/stripe-cancel-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-stripe-cancel-page',
-  templateUrl: './stripe-cancel-page.component.html',
-  styleUrls: ['./stripe-cancel-page.component.scss']
+	selector: 'app-stripe-cancel-page',
+	templateUrl: './stripe-cancel-page.component.html',
+	styleUrls: ['./stripe-cancel-page.component.scss']
 })
 export class StripeCancelPageComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+	isProcessing = false
 
+	withdraw() {}
 }
